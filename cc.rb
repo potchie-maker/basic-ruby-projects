e883356802743ebe989588786e9502a7c33de6b8
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-12-29 01:42:57 -0500

    Add some alternate tooling

--- cc.rb
+++ cc.rb
@@ -1,12 +1,38 @@
+# def caesar_cipher(string, shift)
+#   upp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+#   low = "abcdefghijklmnopqrstuvwxyz"
+
+#   cipher = string.chars.map do |letter|
+#     if upp.include?(letter)
+#       upp[(upp.index(letter) + shift) % 26]
+#     elsif low.include?(letter)
+#       low[(low.index(letter) + shift) % 26]
+#     else
+#       letter
+#     end
+#   end
+
+#   puts "Original: #{string}"
+#   puts "--------------------------------"
+#   puts "Caesar Cipher: #{cipher.join}"
+# end
+# 
+#
+#
 def caesar_cipher(string, shift)
   upp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   low = "abcdefghijklmnopqrstuvwxyz"
 
+  # shift = shift % 26
+
+  u_shift = upp.chars.rotate(shift)
+  l_shift = low.chars.rotate(shift)
+
   cipher = string.chars.map do |letter|
     if upp.include?(letter)
-      upp[(upp.index(letter) + shift) % 26]
+      u_shift[upp.index(letter)]
     elsif low.include?(letter)
-      low[(low.index(letter) + shift) % 26]
+      l_shift[low.index(letter)]
     else
       letter
     end
