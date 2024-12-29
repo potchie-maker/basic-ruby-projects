fd46b657ce9a04e08641bd39611e3bc6a6af6ed6
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-12-29 01:31:46 -0500

    Make syntax changes

--- cc.rb
+++ cc.rb
@@ -2,16 +2,16 @@ def caesar_cipher(string, shift)
   upp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
   low = "abcdefghijklmnopqrstuvwxyz"
 
-  cipher = string.chars
-                 .map do |letter|
-                    if upp.include?(letter)
-                      upp[(upp.index(letter) + shift) % 26]
-                    elsif low.include?(letter)
-                      low[(low.index(letter) + shift) % 26]
-                    else
-                      letter
-                    end
-                end
+  cipher = string.chars.map do |letter|
+    if upp.include?(letter)
+      upp[(upp.index(letter) + shift) % 26]
+    elsif low.include?(letter)
+      low[(low.index(letter) + shift) % 26]
+    else
+      letter
+    end
+  end
+  
   puts "Original: #{string}"
   puts "--------------------------------"
   puts "Caesar Cipher: #{cipher.join}"
