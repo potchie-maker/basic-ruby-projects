f5898d2a03c37e9f94184e5e562146d3c3d69939
potchie-maker <177373729+potchie-maker@users.noreply.github.com>
2024-12-29 05:03:00 -0500

    Complete Sub Strings

--- sub-strings/ss.rb
+++ sub-strings/ss.rb
@@ -1,3 +1,20 @@
 def substrings(word, dictionary)
-  
+  lowered = word.split(' ').map { |string| string.downcase }.join(' ')
+
+  list = dictionary.reduce(Hash.new(0)) do |hash, string|
+    if lowered.include?(string)
+      hash[string] += 1
+    end
+    hash
+  end
+  p list
 end
+
+dictionary = ["below","down","go","going","horn","how","howdy","it","i","low","own","part","partner","sit"]
+
+substrings("below", dictionary)
+# => { "below" => 1, "low" => 1 }
+
+substrings("Howdy partner, sit down! How's it going?", dictionary)
+# => { "down" => 1, "go" => 1, "going" => 1, "how" => 2, "howdy" => 1, "it" => 2, "i" => 3, "own" => 1, "part" => 1, "partner" => 1, "sit" => 1 }
+
